d40367ff4289bf20151b5fdcf7679ee275b9cef4
AkilBlanchard <akilblanchard@github.com>
2023-05-08 23:56:55 -0500

    Configured providers for the project

---
+++ providers.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+    region = "us-east-1"
+  
+}
+
+provider "aws"{
+    alias="us-east-2"
+    region = "us-east-2"
+}
+
+provider "aws"{
+    alias="us-west-3"
+    region = "us-east-3"
+}
