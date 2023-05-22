110d4e9b1a92d22cb40a5f9e7f763f09a99799bc
AkilBlanchard <akilblanchard@github.com>
2023-05-21 22:40:31 -0500

    Created vpc endpoint for US-EAST-1 VPC

--- vpc_endpoints.tf
+++ vpc_endpoints.tf
@@ -33,8 +33,24 @@ resource "aws_security_group" "endpoints_us_east_1"{
     tags = local.common_tags
 }
 
+#us-east-1-Endpoint
 module "endpoints_us_east_1" {
-    
+    source = "terraform-aws-modules/vpc/aws//modules/vpc-endpoints"
+    version = "3.10.0"
+    vpc_id             = module.vpc_us_east_1.vpc_id
+  security_group_ids = [aws_security_group.endpoints_us_east_1.id]
+
+  endpoints = {
+    s3 = {
+      # interface endpoint
+      service             = "s3"
+      subnet_ids = module.vpc_us_east_1.private_subnets
+      tags                = { Name = "s3-vpc-endpoint" }
+    },
+  tags = local.common_tags
 }
+}
+
+
 
 
