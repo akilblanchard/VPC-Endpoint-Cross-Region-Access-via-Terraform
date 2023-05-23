946a7c1afaa466eb5a4515f73ee6ec9cc182366c
AkilBlanchard <akilblanchard@github.com>
2023-05-22 23:11:07 -0500

    Created security group for us-east-2 vpc

--- vpc_endpoints.tf
+++ vpc_endpoints.tf
@@ -50,6 +50,43 @@ module "endpoints_us_east_1" {
   tags = local.common_tags
 }
 }
+#us-east-2
+resource "aws_security_group" "endpoints_us_east_2"{
+    name = "${local.prefix}-endpoints"
+    description = "Allow all HTTPS traffic"
+    vpc = module.vpc_us_east_2.vpc_id
+    ingress = [
+        {
+            description = "HTTPS Traffic"
+            from_port = 443
+            to_port = 443
+            protocol = "tcp"
+            cidr_blocks = ["10.0.0.0/8"]
+            ipv6_cidr_blocks = []
+            prefix_list_ids = []
+            security_groups = []
+            self = false
+        }
+    ]
+    egress = [
+    {
+        description = "ALL Traffic"
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+        ipv6_cidr_blocks = []
+        prefix_list_ids = []
+        security_groups = []
+        self = false
+    }
+    ]
+    tags = local.common_tags
+    provider = {
+        aws = aws.us-east-2
+    }
+}
+
 
 
 
